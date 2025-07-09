8aaa66d607792e590dd1907a0db6dbaba12c7c54
Achinta005 <achintahazra8515@gmail.com>
2025-07-09 23:13:46 +0530

    update

---
+++ frontend/src/pages/admin.js
@@ -0,0 +1,8 @@
+// admin.js
+export default function Admin() {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/project.js
@@ -0,0 +1,8 @@
+// project.js
+export default function Project() {
+  return (
+    <div>
+      <h1>Project Page</h1>
+    </div>
+  );
+}
