d7bbc3e57dc7f3629aa93a2aba262ed12210d378
Achinta005 <achintahazra8515@gmail.com>
2025-07-16 22:15:54 +0530

    env

--- frontend/app/contact/ContactForm.js
+++ frontend/app/contact/ContactForm.js
@@ -12,7 +12,7 @@ const ContactForm2 = () => {
 
   const onSubmit = async (data) => {
     try {
-      const res = await fetch("http://localhost:3001/contact", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
