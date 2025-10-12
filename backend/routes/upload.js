5c20578eff76ef70b779f6d7375cb6ce1722b1ad
Achinta005 <achintahazra8515@gmail.com>
2025-10-12 14:55:34 +0530

    up

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -19,29 +19,37 @@ router.post("/", upload.single("image"), async (req, res) => {
       description,
       order,
     } = req.body;
-
     // Convert buffer to readable stream
     const stream = cloudinary.uploader.upload_stream(
       { resource_type: "image", folder: "Uploaded_Images" },
       async (error, result) => {
         if (error) return res.status(500).json({ error: error.message });
-
         const imageUrl = result.secure_url;
 
+        function convertStringToJsonArray(inputString) {
+          if (!inputString) {
+            return "[]";
+          }
+          const splitArray = inputString.split(",");
+          const trimmedArray = splitArray.map((item) => item.trim());
+          return JSON.stringify(trimmedArray);
+        }
+
+        const json_technologies = convertStringToJsonArray(technologies);
         const [input] = await pool.execute(
           `INSERT INTO project_model(title,description,category,technologies,github_url,live_url,image,order_position,created_at,updated_at) VALUES(?,?,?,?,?,?,?,?,NOW(),NOW())`,
           [
             title,
             description,
             category,
-            technologies,
+            json_technologies,
             githubUrl,
             liveUrl,
             imageUrl,
             order,
           ]
         );
-
+        console.log(`Title : ${title},Description : ${description},Technologies : ${json_technologies},Category : ${category},Github-URL : ${githubUrl},Live-URL : ${liveUrl},Image-URL : ${imageUrl} ,Order : ${order}`)
         res.status(200).json({ message: "Uploaded", project: input });
       }
     );
